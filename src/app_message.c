7239ed9a9f1d8db0e23c84b9075475ba3a25e6ea
jtcgreyfox <thomas.la.brown@gmail.com>
2015-02-14 17:07:00 +1100

    Update 1.1
    Added "Feels Like" Apparent Temperature measurement.
    
    Added useful error message when phone's GPS location cannot be retrieved.
    
    Refactored javascript for a bit better maintainability

--- src/app_message.c
+++ src/app_message.c
@@ -3,11 +3,15 @@
 #define KEY_LOCA 0
 #define KEY_DESC 1
 #define KEY_TEMP 2
+#define KEY_APPT 3
 
 static Window    *s_main_window;
 static TextLayer *headingLayer;
 static TextLayer *middleLayer;
 static TextLayer *bottomLayer;
+static TextLayer *bottomRightLayer;
+static TextLayer *bottomHeaderLayer;
+static TextLayer *bottomRightHeaderLayer;
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   Tuple *t = dict_read_first(iterator);
@@ -15,6 +19,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   char *KEY_LOCA_String = t->value->cstring;
   char *KEY_DESC_String = t->value->cstring;
   char *KEY_TEMP_String = t->value->cstring;
+  char *KEY_APPT_String = t->value->cstring;
   
   while (t != NULL)
   {
@@ -29,6 +34,9 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
       case KEY_TEMP:
         KEY_TEMP_String = t->value->cstring;
         break;
+      case KEY_APPT:
+        KEY_APPT_String = t->value->cstring;
+        break;
     }
     t = dict_read_next(iterator);
   }
@@ -39,7 +47,10 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   
   text_layer_set_text(headingLayer, KEY_LOCA_String);
   text_layer_set_text(middleLayer,  KEY_DESC_String);
+  text_layer_set_text(bottomHeaderLayer, "Actual");
+  text_layer_set_text(bottomRightHeaderLayer, "Feels Like");
   text_layer_set_text(bottomLayer,  KEY_TEMP_String);
+  text_layer_set_text(bottomRightLayer, KEY_APPT_String);
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
@@ -64,28 +75,52 @@ static void main_window_load(Window *window) {
   int middleLayerHeight  = 72;
   int bottomLayerHeight  = 52;
   
-  headingLayer = text_layer_create(GRect(10, 0, window_bounds.size.w-10, headingLayerHeight));
-  text_layer_set_font(headingLayer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  //text_layer_set_text_color(headingLayer, GColorWhite);
-  //text_layer_set_background_color(headingLayer, GColorBlack);
-  text_layer_set_text(headingLayer, "Loading Data...");
-  text_layer_set_overflow_mode(headingLayer, GTextOverflowModeFill);
-  layer_add_child(window_layer, text_layer_get_layer(headingLayer));
-  
-  middleLayer = text_layer_create(GRect(10, headingLayerHeight, window_bounds.size.w-10, middleLayerHeight));
+  middleLayer = text_layer_create(GRect(5, headingLayerHeight-8, window_bounds.size.w-5, middleLayerHeight));
   text_layer_set_font(middleLayer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   //text_layer_set_text_color(middleLayer, GColorWhite);
   //text_layer_set_background_color(middleLayer, GColorBlack);
   text_layer_set_overflow_mode(middleLayer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(middleLayer));
   
-  bottomLayer = text_layer_create(GRect(10, headingLayerHeight + middleLayerHeight, window_bounds.size.w-10, bottomLayerHeight));
+  bottomLayer = text_layer_create(GRect(10, headingLayerHeight + middleLayerHeight+6, (window_bounds.size.w / 2), bottomLayerHeight));
   text_layer_set_font(bottomLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
   //text_layer_set_text_color(bottomLayer, GColorWhite);
   //text_layer_set_background_color(bottomLayer, GColorBlack);
-  text_layer_set_overflow_mode(bottomLayer, GTextOverflowModeFill);
-  //text_layer_set_text_alignment(bottomLayer, GTextAlignmentCenter);
+  //text_layer_set_overflow_mode(bottomLayer, GTextOverflowModeFill);
+  text_layer_set_text_alignment(bottomLayer, GTextAlignmentLeft);
   layer_add_child(window_layer, text_layer_get_layer(bottomLayer));
+  
+  bottomRightLayer = text_layer_create(GRect((window_bounds.size.w / 2), headingLayerHeight + middleLayerHeight+6, (window_bounds.size.w / 2)-10, bottomLayerHeight));
+  text_layer_set_font(bottomRightLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+  //text_layer_set_text_color(bottomRightLayer, GColorWhite);
+  //text_layer_set_background_color(bottomRightLayer, GColorBlack);
+  //text_layer_set_overflow_mode(bottomRightLayer, GTextOverflowModeFill);
+  text_layer_set_text_alignment(bottomRightLayer, GTextAlignmentRight);
+  layer_add_child(window_layer, text_layer_get_layer(bottomRightLayer));
+  
+  bottomHeaderLayer = text_layer_create(GRect(10, headingLayerHeight + middleLayerHeight-2, (window_bounds.size.w / 2), 16));
+  text_layer_set_font(bottomHeaderLayer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  //text_layer_set_text_color(bottomHeaderLayer, GColorWhite);
+  //text_layer_set_background_color(bottomHeaderLayer, GColorBlack);
+  text_layer_set_overflow_mode(bottomHeaderLayer, GTextOverflowModeFill);
+  text_layer_set_text_alignment(bottomHeaderLayer, GTextAlignmentLeft);
+  layer_add_child(window_layer, text_layer_get_layer(bottomHeaderLayer));
+  
+  bottomRightHeaderLayer = text_layer_create(GRect((window_bounds.size.w / 2), headingLayerHeight + middleLayerHeight-2, (window_bounds.size.w / 2)-10, 16));
+  text_layer_set_font(bottomRightHeaderLayer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  //text_layer_set_text_color(bottomRightHeaderLayer, GColorWhite);
+  //text_layer_set_background_color(bottomRightHeaderLayer, GColorBlack);
+  text_layer_set_overflow_mode(bottomRightHeaderLayer, GTextOverflowModeFill);
+  text_layer_set_text_alignment(bottomRightHeaderLayer, GTextAlignmentRight);
+  layer_add_child(window_layer, text_layer_get_layer(bottomRightHeaderLayer));
+  
+  headingLayer = text_layer_create(GRect(5, -5, window_bounds.size.w-5, headingLayerHeight));
+  text_layer_set_font(headingLayer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  //text_layer_set_text_color(headingLayer, GColorWhite);
+  //text_layer_set_background_color(headingLayer, GColorBlack);
+  text_layer_set_text(headingLayer, "Loading Data...");
+  text_layer_set_overflow_mode(headingLayer, GTextOverflowModeFill);
+  layer_add_child(window_layer, text_layer_get_layer(headingLayer));
 }
 
 static void main_window_unload(Window *window) {
@@ -93,6 +128,9 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(headingLayer);
   text_layer_destroy(middleLayer);
   text_layer_destroy(bottomLayer);
+  text_layer_destroy(bottomRightLayer);
+  text_layer_destroy(bottomHeaderLayer);
+  text_layer_destroy(bottomRightHeaderLayer);
 }
 
 static void init() {
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,27 +1,16 @@
-var locationOptions = {
-  enableHighAccuracy: true, 
-  maximumAge: 10000, 
-  timeout: 10000
-};
-function locationSuccess(pos) {
-  var locationMessage = 'lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude;
-  console.log(locationMessage);
-  sendWeatherData(locationMessage, true);
-}
-function locationError(err) {
-  var locationMessage = 'location error (' + err.code + '): ' + err.message;
-  console.log(locationMessage);
-  sendWeatherData(locationMessage, false);
+function pushToPebble(KEY_LOCA, KEY_DESC, KEY_TEMP, KEY_APPT)
+{
+   Pebble.sendAppMessage({
+      'KEY_LOCA': KEY_LOCA,
+      'KEY_DESC': KEY_DESC,
+      'KEY_TEMP': KEY_TEMP,
+      'KEY_APPT': KEY_APPT
+   });
 }
 
-function sendWeatherData(locationMessage, isActualLocation) {
-  var weatherDataUrl = 'http://api.openweathermap.org/data/2.5/weather?';
-  if (!isActualLocation) {
-    weatherDataUrl = weatherDataUrl.concat("q=Sydney,au");
-  } else {
-    weatherDataUrl = weatherDataUrl.concat(locationMessage);
-  }
-  
+function processWeatherDataSuccess(locationMessage)
+{
+  var weatherDataUrl = 'http://api.openweathermap.org/data/2.5/weather?' + locationMessage;
   var request = new XMLHttpRequest();
   console.log(weatherDataUrl);
   request.open('GET', weatherDataUrl, true);
@@ -31,12 +20,15 @@ function sendWeatherData(locationMessage, isActualLocation) {
         console.log(request.responseText);
         var response = JSON.parse(request.responseText);
         if (response) {
-          Pebble.sendAppMessage({
-            'KEY_LOCA':response.name,
-            'KEY_DESC':response.weather[0].description.concat("\nHumidity: ",response.main.humidity,"%\nWind: ",response.wind.speed,"m/s"),
-            'KEY_TEMP':Math.round(response.main.temp - 273.15).toString()
-          });
-          console.log("Weather data sent!");
+            var Name               = response.name;
+            var Temp               = Math.round(response.main.temp - 273.15);
+            var WindSpeed          = response.wind.speed;
+            var Humidity           = response.main.humidity;
+            var WaterVaporPressure = (Humidity / 100 ) * 6.105 * Math.exp((17.27 * Temp) / (237.7 + Temp));
+            var ApparentTemp       = Math.round(Temp + (0.33*WaterVaporPressure) - (0.7*WindSpeed) - 4);
+            var Details            = response.weather[0].description.concat("\nHumidity: ",Humidity,"%\nWind: ",WindSpeed,"m/s");
+            pushToPebble(Name.toString(), Details.toString(), Temp.toString(), ApparentTemp.toString());
+            console.log("Weather data sent!");
         }
       } else {
         console.log("status != 200");
@@ -47,12 +39,31 @@ function sendWeatherData(locationMessage, isActualLocation) {
   };
   request.send(null);
 }
+function processWeatherDataFailure(errorMessage)
+{
+   pushToPebble("ERROR", errorMessage.toString(), "", "");
+   console.log("Weather data ERROR sent!");
+}
 
+var locationOptions = {
+  enableHighAccuracy: true, 
+  maximumAge: 10000, 
+  timeout: 10000
+};
+function locationSuccess(pos)
+{
+  var locationMessage = 'lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude;
+  console.log(locationMessage);
+  processWeatherDataSuccess(locationMessage);
+}
+function locationError(err)
+{
+  var errorMessage = 'error [' + err.code + ']: ' + err.message;
+  console.log(errorMessage);
+  processWeatherDataFailure(errorMessage);
+}
 Pebble.addEventListener('ready', function(e) {
   console.log('PebbleKit JS Ready!');
   console.log('Sending data...');
-  
   navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-  
-  
 });
